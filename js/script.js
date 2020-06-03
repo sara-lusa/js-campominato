a9287e8bcf210470e6101d32b07b790969472027
Sara <saracorsob@gmail.com>
2020-06-03 17:47:17 +0200

    Scrittura + bonus

--- js/script.js
+++ js/script.js
@@ -7,12 +7,26 @@
 // massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte
 // che l’utente ha inserito un numero consentito.
+
 // BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il
 // range di numeri casuali.
 // Con difficoltà 0=> tra 1 e 100, con difficoltà 1 => tra 1 e 80, con difficoltà 2=> tra 1 e 50
 
 // PER BONUS
-var numeroMassimo = 100;
+var difficoltà = prompt('Scegli un livello di difficoltà: 0, 1 o 2?');
+var numeroMassimo;
+
+switch (difficoltà) {
+  case '0':
+    numeroMassimo = 100;
+    break;
+  case '1':
+    numeroMassimo = 80;
+    break;
+  case '2':
+    numeroMassimo = 50;
+    break;
+}
 
 // Genero 16 numeri casuali tra 1 e 100 e li metto in una array
 // dichiaro array
@@ -23,8 +37,8 @@ for (var i = 0; i <= 16; i++ ) {
   var computerRandom = Math.floor(Math.random() * 100) + 1;
   // push nell'array
   numeriComputer.push(computerRandom);
-  console.log(numeriComputer);
 }
+console.log(numeriComputer);
 
 // Chiedo all'utente di inserire un numero alla volta, fino a un massimo di 84 (100 - 16)
 var numeriUtente = [];
@@ -44,13 +58,13 @@ while ( numeroUtenteVietato == false && numeriUtente.length <= (numeroMassimo - 
   else {
     numeriUtente.push(singoloNumeroUtente);
   }
-  console.log(singoloNumeroUtente);
-  console.log(numeriUtente);
 }
+
+console.log(numeriUtente);
 
 // Comunico il punteggio, il numero di volte che ha inserito un numero consentito
 var messaggio = 'Hai vinto!';
 if (numeroUtenteVietato == true) {
-  messaggio = 'Peccato, hai perso. il tuo punteggio è di ' + (numeriUtente.length + 1);
+  messaggio = 'Peccato, hai perso. il tuo punteggio è di ' + (numeriUtente.length);
 }
 console.log(messaggio);
