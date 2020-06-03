65674934e82302f587e80cd1b1c742fe4f8ce9f7
Sara <saracorsob@gmail.com>
2020-06-03 15:47:24 +0200

    Aggiunta commenti italiano + scaletta da seguire

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
-// // DEBUG: 
-alert('Ciao');
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// In seguito deve chiedere all’utente di inserire un numero alla volta, sempre
+// compreso tra 1 e 100.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si
+// continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero
+// massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte
+// che l’utente ha inserito un numero consentito.
+// BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il
+// range di numeri casuali.
+// Con difficoltà 0=> tra 1 e 100, con difficoltà 1 => tra 1 e 80, con difficoltà 2=> tra 1 e 50
+
+
+// Genero 16 numeri casuali tra 1 e 100 e li metto in una array
+// dichiaro array
+for (16) {
+  // Genero numero random
+  // push nell'array
+}
+
+// Chiedo all'utente di inserire un numero alla volta, fino a un massimo di 84 (100 - 16)
+while () {
+  prompt
+  if (insides) {
+    // si ferma
+  }
+  push
+}
+// Controllo se il numero è tra quelli generati inizialmente, nel caso termino la partita.
+
+// Comunico il punteggio, il numero di volte che ha inserito un numero consentito
